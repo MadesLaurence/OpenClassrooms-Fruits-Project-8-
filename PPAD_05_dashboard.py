e990c34f2037e32a37a9ab175326ef32a8936d73
MadesLaurence <laurence.mades@bnpparibas.com>
2022-02-13 17:24:34 +0100

    Initialisation

--- PPAD_05_dashboard.py
+++ PPAD_05_dashboard.py
@@ -3,9 +3,8 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import numpy as np
 import pandas as pd
-#import matplotlib
-#from matplotlib import figure
-#import matplotlib.pyplot as plt
+from matplotlib import figure
+import matplotlib.pyplot as plt
 
 
 ############### Déclaration des constantes #################################################### 
